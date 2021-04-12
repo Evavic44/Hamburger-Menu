17435897251e7625caa53b889241d949e1513d2b
Eke Victor <evavic44@gmail.com>
2021-04-12 06:36:25 +0100

    Added Hamburger Script

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+conts navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
