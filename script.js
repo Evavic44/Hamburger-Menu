3655d470f0eebbd2730a9a3d0723b58cf41a62f3
Eke Victor <evavic44@gmail.com>
2021-04-13 23:57:59 +0100

    Final Changes

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
